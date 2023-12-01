3f1ade8bc14fe4e449aa4c31b64dfd06c188c42f
f <franklinrohan2002@gmail.com>
2023-12-01 22:30:28 +0530

    connect to mongoDB Atlas

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const password = process.env.MONGO_PASSWORD;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(
+  `mongodb+srv://franklinrohan:${password}@cluster0.etmg3so.mongodb.net/Cluster0?retryWrites=true&w=majority`
+);
 
 app.listen(2001, () => {
   console.log("Listening to port 2001");
