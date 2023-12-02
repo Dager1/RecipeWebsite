baa624b299fc446d37f87675df15f7db55a23484
f <franklinrohan2002@gmail.com>
2023-12-03 00:25:16 +0530

    imported router module.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello </h1>
+      <Router>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
     </div>
   );
 }
