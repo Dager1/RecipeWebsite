99724fcb647a06d061997b84732b4e3105e08f51
f <franklinrohan2002@gmail.com>
2023-12-02 21:59:36 +0530

    api registeration end point

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, require: true, unique: true },
-  passwor: { type: String, require: true },
+  password: { type: String, require: true },
 });
 
 export const UserModel = mongoose.model("users", UserSchema);
