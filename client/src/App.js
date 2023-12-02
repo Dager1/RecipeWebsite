bdce05ad700287132b769ed74bc2ea16e64dcea3
f <franklinrohan2002@gmail.com>
2023-12-03 00:47:38 +0530

    differnet pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Auth } from "./pages/auth";
+import { CreateRecipe } from "./pages/create-recipe";
+import { SaveRecipe } from "./pages/save-recipe";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/save-recipe" element={<SaveRecipe />} />
         </Routes>
       </Router>
     </div>
